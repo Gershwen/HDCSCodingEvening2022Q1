f915a2cc7cf5f867e1e40055c70104bbfe006a88
Gershwen <76742941+Gershwen@users.noreply.github.com>
2022-03-21 20:59:39 +0200

    solved challenge 1 and 2

--- codingNightChallenge.js
+++ codingNightChallenge.js
@@ -1,11 +1,14 @@
-// For this challenge, you will be required to build a Morse code encryptor and decrypter.
+// You can find a table of Morse code characters here:
+// https://morsedecoder.com/
 
-let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let alphabet = ` !ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
 //splitting alphabet to form an array of seperate elements
 let alphabetArray = alphabet.split("");
 
 //morse alphabet
 let morseCode = [
+  "/",
+  "-.-.--",
   ".-",
   "-...",
   "-.-.",
@@ -34,34 +37,55 @@ let morseCode = [
   "--..",
 ];
 
-// const stringToMorseCode = (word) => {
-//   //store word in own array with each element split
-//   let wordArray = word.split("");
-//   //search through alphabetArray to find matching index
-//   const findIndex = (alphabet) => {
-//     for (let i = 0; i < alphabet.length; i++) {
-//       if (alphabet[i] === wordArray[i]) {
-//         indexes.push(i);
-//       }
-//     }
-//   };
-
-//   findIndex(alphabetArray)
-//   console.log(indexes);
-// };
-
-const stringToMorseCode = (word, alphabetArray, morseCode) => {
+const stringToMorseCode = (word) => {
   let wordArray = word.split("");
+  let index,
+    indexes = [],
+    morseCodedArray = [],
+    morseCodedString;
 
-  //   const filteredArray = wordArray.filter((value) =>
-  //     alphabetArray.includes(value)
-  //   );
-  //need to match the elements in both arrays then find the index of those arrays. Map method?
-  console.log(
-    alphabetArray.map(function (value, index, arr) {
-      return index;
-    })
-  );
+  //below loops through wordArray and for every element it searches alphetArray to find the matching elements index
+  for (let i = 0; i < wordArray.length; i++) {
+    index = alphabetArray.findIndex((letter) => letter === wordArray[i]);
+    indexes.push(index);
+  }
+  //use indexes to match indexes inside morseCode array to extract morecode elements
+  for (let j = 0; j < indexes.length; j++) {
+    //below line sends morse code values to morseCodedArray
+    morseCodedArray.push(morseCode[indexes[j]]);
+    //morseCodedArray to string
+    morseCodedString = morseCodedArray.join(" ");
+  }
+
+  console.log(morseCodedString);
 };
 
-stringToMorseCode("CHALLENGE", alphabetArray, morseCode);
+stringToMorseCode("HELLO MY NAME IS GERSHWEN");
+
+//Challenge 2
+const morseCodeToString = (code) => {
+  //seperating morse code string with by space and storing them as elements inside codeArray
+  let codeArray = code.split(" ");
+  let index,
+    indexes = [],
+    stringedArray = [],
+    wordString;
+
+  for (let i = 0; i < codeArray.length; i++) {
+    index = morseCode.findIndex((morsecode) => morsecode === codeArray[i]);
+    indexes.push(index);
+  }
+
+  for (let j = 0; j < indexes.length; j++) {
+    stringedArray.push(alphabetArray[indexes[j]]);
+    wordString = stringedArray.join("");
+  }
+
+  console.log(wordString);
+};
+
+morseCodeToString(
+  ".... . .-.. .-.. --- / -- -.-- / -. .- -- . / .. ... / --. . .-. ... .... .-- . -."
+);
+
+
